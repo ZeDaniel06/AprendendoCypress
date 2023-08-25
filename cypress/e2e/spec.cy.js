fa78124eb883723d70e22305b62ba74233b46a97
ZeroMeia123 <silvadan752@gmail.com>
2023-08-24 21:52:48 -0300

    fazendo mais verificações em torno da pesquisa do google

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,7 +10,12 @@
 describe('Google test', () =>{
   it('Pesquisar',()=>{
     cy.visit("https://www.google.com")
-    cy.get("#APjFqb").type("Noticias{enter}",{delay:100})
+    cy.url().should('include','google.com')
+
+    cy.get("#APjFqb").type("Noticiassss{backspace}{backspace}{backspace}",{delay:100})
+    cy.get("#APjFqb").should('have.value','Noticias')
+    cy.get("#APjFqb").type("{enter}")
+    cy.url().should('include','Noticias')
     
   })
 })
