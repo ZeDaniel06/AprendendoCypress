de5a551db4948e448fa82c0b49c123da4c239856
ZeroMeia123 <silvadan752@gmail.com>
2023-08-27 18:19:09 -0300

    implementado aliases

--- cypress/e2e/0 - google-search/entrandoVimm.cy.js
+++ cypress/e2e/0 - google-search/entrandoVimm.cy.js
@@ -2,15 +2,16 @@ describe("Testes de Pesquisa por um site",() =>{
 
     beforeEach(()=>{
         cy.visit('https://www.google.com');
+        cy.get('#APjFqb').as('barraDeBusca');
     })
     
     it("Pesquisando o site",()=>{
-        cy.get('#APjFqb').type("Vimm's Lair{enter}");
+        cy.get('@barraDeBusca').type("Vimm's Lair{enter}");
         cy.contains('https://vimm.net').click();
     });
 
     it('Pesquisando o site estou com sorte',()=>{
-        cy.get('#APjFqb').type("Vimm's Lair");
+        cy.get('@barraDeBusca').type("Vimm's Lair");
         cy.get('input[name="btnI"]').eq(0).click();
         cy.origin("https://vimm.net",()=>{
             cy.get('body').contains("Vimm's Lair");
--- cypress/e2e/0 - google-search/spec.cy.js
+++ cypress/e2e/0 - google-search/spec.cy.js
@@ -2,18 +2,19 @@ describe('Google test', () =>{
 
   beforeEach(()=>{
     cy.visit("https://www.google.com")
+    cy.get("#APjFqb").as('barraBusca');
   });
 
  it('Testando pesquisa no Google',()=>{
-  cy.get("#APjFqb").type("Noticiassss{backspace}{backspace}{backspace}",{delay:100})
-  cy.get("#APjFqb").should('have.value','Noticias')
-  cy.get("#APjFqb").type("{enter}")
+  cy.get("@barraBusca").type("Noticiassss{backspace}{backspace}{backspace}",{delay:100})
+  cy.get("@barraBusca").should('have.value','Noticias')
+  cy.get("@barraBusca").type("{enter}")
   cy.url().should('include','Noticias')
   
 })
 
 it('Busca no Google por UEPB - estou com sorte', () => {
-  cy.get("#APjFqb").type('UEPB');
+  cy.get("@barraBusca").type('UEPB');
   cy.get('input[name="btnI"]').eq(0).click();
   cy.origin('https://uepb.edu.br', () => { 
       cy.get('body').contains('UEPB');
@@ -22,7 +23,7 @@ it('Busca no Google por UEPB - estou com sorte', () => {
 
 it('Busca no Google por UEPB - busca google', () => {
   cy.visit('https://www.google.com')
-  cy.get('#APjFqb').type('UEPB');
+  cy.get('@barraBusca').type('UEPB');
   cy.get('input[name="btnK"]').eq(0).click();
   cy.get('body').contains('UEPB');
 });
